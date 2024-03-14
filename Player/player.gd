8228a3bf0a6d313ab3249c49a6b611f4338e3455
nlond <nlondhe@clemson.edu>
2024-03-14 11:46:10 -0400

    Added simple background and character movement (can change sprites later)

---
+++ Player/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+var movement_speed = 250.0
+
+func _physics_process(delta):
+	movement()
+	
+func movement():
+	var x_move = Input.get_action_strength("Right") - Input.get_action_strength("Left")
+	var y_move = Input.get_action_strength("Down") - Input.get_action_strength("Up")
+	var move = Vector2(x_move, y_move)
+	velocity = move.normalized() * movement_speed
+	move_and_slide()
