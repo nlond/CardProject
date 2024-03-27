10f8711395146ef17f3520081bc29016cc5bf0bf
nlond <nlondhe@clemson.edu>
2024-03-27 10:55:51 -0400

    Added functionality to flip sprites based on directional movement

--- Player/player.gd
+++ Player/player.gd
@@ -1,13 +1,23 @@
 extends CharacterBody2D
 
+# adjust player speed with this var
 @export var movement_speed = 150.0
+@onready var sprite = $Sprite2D
 
 func _physics_process(delta):
 	movement()
 	
 func movement():
+	# player movement with WASD
 	var x_move = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	var y_move = Input.get_action_strength("Down") - Input.get_action_strength("Up")
 	var move = Vector2(x_move, y_move)
+	
+	# flip player direction to face direction of movement
+	if move.x > 0:
+		sprite.flip_h = false
+	elif move.x < 0:
+		sprite.flip_h = true
+	
 	velocity = move.normalized() * movement_speed
 	move_and_slide()
--- enemy_slime.gd
+++ enemy_slime.gd
@@ -1,10 +1,18 @@
 extends CharacterBody2D
 
 @export var movement_speed = 20.0
-
 @onready var player = get_tree().get_first_node_in_group("Player")
+@onready var sprite = $Sprite2D
 
 func _physics_process(delta):
+	
+	# enemy always moves in direction of player
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * movement_speed
 	move_and_slide()
+	
+	# flip enemy direction to face player
+	if direction.x > 0.1:
+		sprite.flip_h = true
+	elif direction.x < -0.1:
+		sprite.flip_h = false
